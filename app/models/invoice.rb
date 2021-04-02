68051573c2157a1acf456e4af0a301e4c411bd00
Khoa Nguyen <khoang323@hotmail.com>
2021-04-02 10:24:27 -0600

    Completed created controllers, models, and model tests

---
+++ app/models/invoice.rb
@@ -0,0 +1,9 @@
+class Invoice < ApplicationRecord
+  belongs_to :customer
+  belongs_to :merchant
+  has_many :transactions, dependent: :destroy
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items
+
+  enum status: [:in_progress, :cancelled, :completed]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+
+      resources :customers
+      resources :invoice_items
+      resources :transactions
+      
+      resources :merchants do
+        resources :invoices
+        resources :items
+      end
+    end
+
+  end
 end
